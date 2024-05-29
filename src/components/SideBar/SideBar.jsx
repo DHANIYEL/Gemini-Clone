417788a2b7ce7a8190dc5aa1f4cb95b0e5211c5c
DHANIYEL <dhaniyeldarvesh7@gmail.com>
2024-05-30 00:42:07 +0530

    error

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,41 +1,46 @@
+import { useState } from 'react'
 import {assets} from '../../assets/assets'
 import '../../index.css'
 
 
 const SideBar = () => {
+
+const [extend, setExtend] = useState(true)
+
   return (
     <div className='inline-flex h-full flex-col justify-between bg-[#f0f4f9] sidebar py-6 px-4'>
     <div className='top'>
-      <div className=''>
         <div>
           <img src={assets.menu_icon} className='cursor-pointer block ml-3' alt="menu_icon" />
-          <div className='flex gap-4 items-center mt-12  bg-[#e6eaf1] py-1 px-[10px] rounded-full text-[14px] cursor-pointer text-gray-700'>
+          <div className='inline-flex gap-4 items-center mt-12  bg-[#e6eaf1] py-1 px-3 rounded-full text-[14px] cursor-pointer text-gray-700'>
 
             <img src={assets.plus_icon} alt="plus_icon" />
-            <p>New Chat</p>
+            {extend?<p>New Chat</p>:null} 
           </div>
-          <div className='recent inline-flex flex-col'>
+            {extend ?
+            
+            <div className='recent inline-flex flex-col'>
               <p className='recent-title mt-8 mb-3'>Recent</p>
               <div className='recent-entry '>
               <img src={assets.message_icon} alt="message_icon" />
                 <p>Whats is React.</p>
             </div>
-          </div>
-        </div>
+          </div> : null}
       </div>
     </div>
-    <div className="bottom pr-3 cursor-pointer">
-      <div className='recent-entry'> 
+    <div className="bottom flex flex-col pr-3 cursor-pointer ">
+      <div className='recent-entry '> 
         <img src={assets.question_icon} alt="" />
-        <p>Help</p>
+       {extend? <p>Help</p> : null} 
       </div>
       <div className='recent-entry'>
         <img src={assets.history_icon} alt="" />
-        <p>Activity</p>
+        <p></p>
+       {extend? <p>Activity</p> : null} 
       </div>
       <div className='recent-entry'>
         <img src={assets.setting_icon} alt="" />
-        <p>Settings</p>
+        {extend? <p>Settings</p> : null} 
       </div>
     </div>
     </div>

8ddc62b8d5c20b9f3f2517ea828370cecef8a848
DHANIYEL <dhaniyeldarvesh7@gmail.com>
2024-05-31 01:44:04 +0530

    marging errr

--- src/components/Main/mainBar.jsx
+++ src/components/Main/mainBar.jsx
@@ -15,28 +15,48 @@ const MainBar = () => {
 
 
       {/* MAIN CONTENT */}
-      <div className=" max-w-[900px] mx-auto ">
+      <div className=" max-w-[900px] mx-auto  ">
         <div className="text-[56px] my-12 p-5 font-medium text-[#c4c7c5]">
           <p> <span className=" bg-clip-text	text-transparent bg-gradient-to-r from-[#4b90ff] to-[#ff5546] 	">Hello, DHANIYEL</span></p>
           <p>How can I help you today?</p>
         </div>
-        <div className="card">
+        <div className="grid grid-cols-4 max-sm:grid-cols-2 p-5 gap-5 text-lg text-[#585858] mb-6">
+        <div className="h-[200px] bg-[#f0f4f9] p-4 rounded-lg shadow-sm cursor-pointer relative  hover:bg-[#dfe4ea]">
           <p>Evaluate and rank common camera categories</p>
-          <img src={assets.bulb_icon} alt="" />
+          <img src={assets.bulb_icon} className="w-8 absolute right-3 bottom-3 rounded-full bg-white p-1" alt="" />
         </div>
-        <div className="card">
+        <div className="h-[200px] bg-[#f0f4f9] p-4 rounded-lg shadow-sm cursor-pointer relative hover:bg-[#dfe4ea]">
           <p>Brainstorm presentation ideas about a topic</p>
-          <img src={assets.compass_icon} alt="" />
+          <img src={assets.compass_icon} className="w-8 absolute right-3 bottom-3 rounded-full bg-white p-1" alt="" />
         </div>
-        <div className="card">
+        <div className="h-[200px] bg-[#f0f4f9] p-4 rounded-lg shadow-sm cursor-pointer relative hover:bg-[#dfe4ea]">
           <p>Come up with a product name for a new app</p>
-          <img src={assets.message_icon} alt="" />
+          <img src={assets.message_icon} className="w-8 absolute right-3 bottom-3 rounded-full bg-white p-1" alt="" />
         </div>
-        <div className="card">
+        <div className="h-[200px] bg-[#f0f4f9] p-4 rounded-lg shadow-sm cursor-pointer relative hover:bg-[#dfe4ea]">
           <p>Come up with a complex word riddle, including hints</p>
-          <img src={assets.code_icon} alt="" />
+          <img src={assets.code_icon} className="w-8 absolute right-3 bottom-3 rounded-full bg-white p-1" alt="" />
         </div>
+        </div>
+
+      {/* SEARCH BOX */}
+
+      <div className="m-auto py- absolute bottom-0 max-w-[900px] w-full">
+        <div className="flex items-center justify-between gap-5 px-3 py-4 bg-[#f0f4f9] rounded-full">
+          <input type="text" placeholder="Enter a prompt here" className="border-none outline-none bg-transparent px-3 flex-auto text-lg"/>
+          <div className="flex gap-3  items-center"> 
+            <img src={assets.gallery_icon} className="cursor-pointer w-6" alt="" />
+            <img src={assets.mic_icon} className="cursor-pointer w-6" alt="" />
+            <img src={assets.send_icon} className="cursor-pointer w-6" alt="" />
+          </div>
+        </div>
+        <p className="mx-auto my-2 text-center">Gemini may display inaccurate info, including about people, so double-check its responses. Your privacy & Gemini Apps </p>
       </div>
+
+
+      </div>
+
+
     </div>
   )
 }
